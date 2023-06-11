967452dcf6c7766e68dfbd5c4a19a3cf3a873e93
hayhaydz <haydon1900@gmail.com>
2023-06-11 12:50:22 +0100

    Completed: CSS only transitory implementation

--- source/Background/index.ts
+++ source/Background/index.ts
@@ -4,3 +4,30 @@ import {browser} from 'webextension-polyfill-ts';
 browser.runtime.onInstalled.addListener((): void => {
   console.emoji('🦄', 'extension installed');
 });
+
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab):void => {
+  if (changeInfo.status === "complete") {
+    browser.tabs.insertCSS(tabId, {
+      code: `
+        /* TOP BAR CLEAN CLUTTER | GENERAL LINKS | NOTIFICATIONS | NOTIFICATIONS | */
+        header .nOssi, header .rZMhb:nth-of-type(3), header .kG7WW { display: none !important }
+
+        /* HIDE VIEWS AND DOWNLOADS COUNT */
+        .bck87{ display: none !important; }
+        .Y6uS4.moj5G{ gap: 0px !important; }
+
+        /* HIDE INFO BUTTON */
+        #popover-photo-footer-info { display: none !important; }
+
+        /* HIDE MENU STATS BUTTON */
+        header #popover-avatar-loggedin-menu ul:first-of-type > li:nth-of-type(3) { display: none !important; }
+
+        /* HIDE PROFILE STATS BUTTON */
+        #app > div > div:nth-of-type(2) > div:nth-of-type(3) > div > div > div > ul > li:last-of-type { display: none !important; }
+
+        /* HIDE WHOLE STATS PAGE */
+        ${tab.url?.includes("stats") ? `#app > div > div:nth-of-type(2) > div:nth-of-type(4) { display: none !important; }` : ''}
+      `
+    });
+  }
+})
--- source/ContentScript/index.ts
+++ source/ContentScript/index.ts
@@ -1,6 +1,22 @@
-console.log('hello from content script');
-console.log(document.getElementsByClassName('XWQyM'));
-document.body.addEventListener('DOMNodeInserted', () => {
-  console.log(document.getElementsByClassName('XWQyM'));
-}, false );
+// console.log(document.getElementsByClassName('XWQyM'));
+// document.body.addEventListener('DOMNodeInserted', () => {
+//   console.log(document.getElementsByClassName('XWQyM'));
+// }, false );
+
+// MORE RELIABLE BUT PERFORMANCE COSTLY
+// let menuContainer = document.getElementById("popover-avatar-loggedin-menu");
+// let menuButton = menuContainer?.getElementsByTagName("button");
+
+// menuContainer?.addEventListener('DOMNodeInserted', () => {
+//   let menuLinks = menuContainer?.getElementsByTagName("a");
+//   if(!menuLinks) return
+//   for(let i = 0; i < menuLinks.length; i++) {
+//     console.log(menuLinks[i].href)
+//     if(menuLinks[i].href.includes("stats")) {
+//       console.log('stats link found')
+//       menuLinks[i].style.display = 'none'
+//     }
+//   }
+// }, false );
+
 export {};
